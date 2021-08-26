718eb6f8c5bbe3cb2f8a4778b36ef87eceed0548
Firdevs Akbayır <77115759+lyushher@users.noreply.github.com>
2021-08-26 13:20:30 +0300

    Update package_index.py

--- package_index.py
+++ package_index.py
@@ -88,7 +88,7 @@ def egg_info_for_url(url):
     parts = urllib.parse.urlparse(url)
     scheme, server, path, parameters, query, fragment = parts
     base = urllib.parse.unquote(path.split('/')[-1])
-    if server == 'sourceforge.net' and base == 'download':  # XXX Yuck
+    if server == 'sourceforge.net' and base == 'download':  
         base = urllib.parse.unquote(path.split('/')[-2])
     if '#' in base:
         base, fragment = base.split('#', 1)
